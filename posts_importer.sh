2244b463bb8e108369660a365e918e6ae835cb1c
whodafuckisthisguy <72853684+madmandtt@users.noreply.github.com>
2023-12-04 22:40:24 +0700

    test jekyll-notion

---
+++ posts_importer.sh
@@ -0,0 +1,4 @@
+#! bin/sh
+
+export NOTION_TOKEN=secret_jaLIqA9uQ8wL08XFUz5IdcZuNtU7EhojV44Xrqb7ViI
+notion2md --download -n notion_posts -p ./_posts -u https://www.notion.so/e46/63dbdd02b59546c7a4906c9ad2243fd4?v=9f789b4954ff4058b49ec586689c5db5&pvs=4
